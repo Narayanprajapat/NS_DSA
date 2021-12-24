9ee978eaa6c56ca15ee24264e210e3421bddbcdf
Narayanprajapat <narayanprajapat00099@gmail.com>
2021-12-25 00:47:20 +0530

    Linear Search

--- src/Searching/Linear.java
+++ src/Searching/Linear.java
@@ -12,13 +12,17 @@ public class Linear {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number Of Element : ");
+
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        int[] arr = new int[n];
+
         System.out.println("Enter Element : "+n);
-        for(int i=0;i<n;i++){
+
+        for(int i=0;i<n;i++)
             arr[i] = sc.nextInt();
-        }
+
         System.out.println("Enter Searching Of Element : ");
+
         int key = sc.nextInt();
         System.out.println(Linear_Search(arr,n,key));
     }
