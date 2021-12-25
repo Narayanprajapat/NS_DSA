fefbd9e8fd529e63d59f892a2f60983d3c340c28
Narayanprajapat <narayanprajapat00099@gmail.com>
2021-12-25 10:35:22 +0530

    Hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello Word");
+
     }
 
 }
-//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
+//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
